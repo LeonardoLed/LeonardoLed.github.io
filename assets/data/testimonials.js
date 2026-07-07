de0c887115b4f26f865d1b84b14454563dab29e9
Leonardo Ledesma <leonardoledd@gmail.com>
2026-07-07 15:04:17 -0600

    new testimonials

--- assets/data/testimonials.js
+++ assets/data/testimonials.js
@@ -9,12 +9,21 @@ window.CVLEO_DATA.testimonials = [
     text: 'He identificado en Leonardo diversas cualidades que lo posicionan como un referente en su disciplina. Profesional y proactivo, con una mirada siempre crítica y dirigida hacia realizar muy buenas aportaciones. Le brinda un sentido humano a su trabajo alineado con sus principios éticos y morales que lo hacen una gran persona.',
     photo: 'assets/img/testimonials/erik.jpeg'
   },
+
+  {
+    name: 'Andrea Uxué Amaya Navarrete',
+    role: 'Desarrolladora Full-Stack',
+    company: 'CPESGI, UNAM',
+    text: 'Leonardo es una persona excepcional, tanto en lo profesional como en lo personal. Su liderazgo se destaca por la claridad en la comunicación y la capacidad de motivar a los demás. Siempre está dispuesto a colaborar, escuchar y ofrecer su apoyo cuando alguien lo necesita. También mantiene un ambiente de trabajo positivo y respetuoso, promoviendo el trabajo en equipo y el crecimiento.',
+    photo: 'assets/img/testimonials/andrea.jpg'
+  }
+
   {
     name: 'Yóas S. Ramírez Graullera',
     role: 'Rsponsable del Laboratorio de Investigación y Desarrollo de Aplicaciones Interactivas para la Neuro-Rehabilitación',
     company: 'Instituto de Fisologia Celular, UNAM',
     text: 'He convivido con él desde hace más de 13 años y puedo asegurar que es una de las personas más responsables, perseverantes y comprometidas que conozco. No omito resaltar que constantemente busca superarse en lo humano y lo académico.',
-    photo: 'assets/img/testimonials/yoas.jpg'
+    photo: 'assets/img/testimonials/yoas.jpeg'
   },
   {
     name: 'Diana Giselle Hidalgo López',
